2fdc959378167ed65d15d717d0095b752c307f63
flazepe <51487578+flazepe@users.noreply.github.com>
2021-03-04 17:12:43 +0800

    Update typings, README, and fix "rough emoji"

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,18 @@
 import { CanvasRenderingContext2D } from "canvas";
 
 declare module "node-canvas-with-twemoji-and-discord-emoji" {
+	export interface DrawOptions {
+		maxWidth?: number;
+		emojiSideMarginPercent?: number;
+		emojiTopMarginPercent?: number;
+	}
+	
 	export function fillTextWithTwemoji(
 		context: CanvasRenderingContext2D,
 		text: string,
 		x: number,
 		y: number,
-		options?: any
+		options?: DrawOptions
 	): Promise<void>;
 	
 	export function strokeTextWithTwemoji(
@@ -14,12 +20,12 @@ declare module "node-canvas-with-twemoji-and-discord-emoji" {
 		text: string,
 		x: number,
 		y: number,
-		options?: any
+		options?: DrawOptions
 	): Promise<void>;
 	
 	export function measureText(
 		context: CanvasRenderingContext2D,
 		text: string,
-		options?: any
+		options?: { emojiSideMarginPercent?: number; }
 	): { width: number; alphabeticBaseline: number; };
 }
